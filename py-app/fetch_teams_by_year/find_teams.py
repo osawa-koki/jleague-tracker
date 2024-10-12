50a98c9641f1199ddb8fdfa8bb934bd4f92f9baf
osawa-koki <koki@osawa.dev>
2024-10-13 00:41:11 +0900

    正式名称と略称のデータを用意して、コードを修正。

--- py-app/fetch_teams_by_year/find_teams.py
+++ py-app/fetch_teams_by_year/find_teams.py
@@ -1,10 +1,31 @@
+import json
+from pathlib import Path
 from bs4 import BeautifulSoup, Tag
 
+from typing import TypedDict
 
-def find_teams(html: str) -> list[str]:
+
+class TeamName(TypedDict):
+    long_name: str
+    short_name: str
+    english_name: str
+
+
+with open(Path(__file__).parent / "./name_mapper.json", "r") as f:
+    name_mapper: list[TeamName] = json.load(f)
+
+
+def find_teams(html: str) -> list[TeamName]:
     soup: Tag = BeautifulSoup(html, "html.parser")
     table: Tag | None = soup.select_one("table.infoTable")
     if table is None:
         return []
     teams = [th.text.strip() for th in table.select("tr th")]
-    return teams
+    team_names: list[TeamName] = []
+    for team in teams:
+        matched_team = next((item for item in name_mapper if item["long_name"] == team), None)
+        if matched_team is not None:
+            team_names.append(matched_team)
+        else:
+            print(f"No match found for team: {team}")
+    return team_names
