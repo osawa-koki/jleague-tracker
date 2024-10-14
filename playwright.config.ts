751acd05f48de3637da983cd657ef9af41c28961
osawa-koki <koki@osawa.dev>
2024-10-14 17:24:32 +0900

    テストのリファクタリング。

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'yarn server',
     url: 'http://127.0.0.1:8000',
     reuseExistingServer: !isCi
   }
